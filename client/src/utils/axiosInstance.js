58bfe4d39db9612ef132768d8f67635adae9dd2f
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2024-11-10 23:25:52 +0700

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,29 +1,24 @@
 import axios from "axios";
-
+import TokenService from "../features/token/tokenService"; // Adjust the import path as necessary
 
 // Create an axios instance with common config
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Replace with your FastAPI base URL
-  timeout: 1000,
+  baseURL: "http://localhost:8000", 
   headers: { "Content-Type": "application/json" },
 });
 
+// Function to set up Axios interceptors
 export const setupAxiosInstance = () => {
- // Get token from Redux store
-  
   axiosInstance.interceptors.request.use(
-
-        //   if (!token) {
-    //     config.headers["Authorization"] = `Bearer ${accessToken}`;
-    //   } else {
-    //     config.headers["Authorization"] = `Bearer ${token}`;
-    //   }
-    (config) => {
-
-      // const token = store.getState()?.token?.value
-    
-      config.headers["Authorization"] = `Bearer ${token}`; // Dynamically set the token
+    async (config) => {
+      // Get the token from the Redux store or TokenService
+      const token = await TokenService.accessToken();
       
+      // If a token exists, add it to the Authorization header
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+
       return config;
     },
     (error) => Promise.reject(error)
@@ -33,6 +28,3 @@ export const setupAxiosInstance = () => {
 };
 
 export default axiosInstance;
-
-
-
